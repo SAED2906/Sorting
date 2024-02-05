26da37dc7db8c179315f4a81933af15dd0ed0b57
William Marais <79162124+SAED2906@users.noreply.github.com>
2024-02-05 21:48:56 +0200

    Update stalinsort-no-comparisons.py

--- stalinsort-no-comparisons.py
+++ stalinsort-no-comparisons.py
@@ -3,7 +3,7 @@ arr = [1,2,3,3,5,1,6,8]
 temp = 0
 ls = []
 for x in arr:
-    c = x-temp
+    c = x-temp # Turn this line into: c = int(10*(x-temp))  this will allow for floats, adjust the multiplier as needed ig
     try:
         s = (c >> 31) & 1
         c /= c
